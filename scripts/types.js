12e0bd6c7965f1fbbd6bde929bb0fe2f38350678
Jem Pimentel <jem.pimentel@outlook.com>
2019-12-28 02:04:02 +0800

    Numbers

--- scripts/types.js
+++ scripts/types.js
@@ -1,9 +1,11 @@
-const name = 'Jem';
-const hello = 'Hello world!';
-const html = `
-<div>
-  <h2>${name}</h2>
-  <p>${hello}</p>
-</div>
-`;
-document.body.innerHTML = html;
+const age = 9.5;
+console.log(age, typeof age);
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+const dadGets = smarties % kids;
+console.log(`Each kid gets ${eachKidGets}, dad gets ${dadGets}`);
+
+const power = 10 ** 20000;
+console.log(power);
