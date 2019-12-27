4d22bd96aaa1d0c4fe457c791a5828567ad93143
Jem Pimentel <jem.pimentel@outlook.com>
2019-12-28 02:11:05 +0800

    Objects

--- scripts/types.js
+++ scripts/types.js
@@ -1,11 +1,6 @@
-const age = 9.5;
-console.log(age, typeof age);
-
-const smarties = 20;
-const kids = 3;
-const eachKidGets = Math.floor(smarties / kids);
-const dadGets = smarties % kids;
-console.log(`Each kid gets ${eachKidGets}, dad gets ${dadGets}`);
-
-const power = 10 ** 20000;
-console.log(power);
+const person = {
+  firstName: 'Jem',
+  lastName: 'Pimentel',
+  age: 7
+};
+console.log(typeof person);
